dedc1820fc00b3416bcbe6e31f595ec3cfd68f85
Johnson Dinh <dinhjd@uci.edu>
2022-07-12 17:33:31 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Date.hpp"
 #include "matplotlib-cpp/matplotlibcpp.h"
 #include "xtensor.hpp"
+#include "fstream"
 
 
 namespace plt = matplotlibcpp;
@@ -20,7 +21,26 @@ int main() {
 //    api.download_ticker_data("GME", starting_date, current_date);
 //    api.set_interval(DAILY);
 
-    // read file and create xtensor arrays
+    std::ifstream file;
+    file.open("GME_1657598230.csv");
+
+    auto data = xt::load_csv<std::string>(file);
+    // std::cout << data << std::endl;
+
+    std::vector<std::string> dates = {};
+    unsigned int _shape = data.shape(0);
+
+    for (int i = 1; i < _shape; i++)
+        dates.push_back(data(i, 0));
+
+    std::vector<double> open_data = {};
+    for (int i = 1; i < _shape; i++) {
+        double val = std::stod(data(i, 1));
+        open_data.push_back(val);
+    }
+
+    plt::plot(open_data);
+    plt::show();
 
 
 
